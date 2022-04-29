59b24b260b80521735d31eba56df517ce209b754
devgru-3-2 <bharatha.19cs@saividya.ac.in>
2022-04-29 15:02:55 +0530

    our initial api

---
+++ quotes-api-server/main2.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type quote struct {
+	Title  string
+	Author string
+}
+
+//var quotes []quote = make([]quote, 0)
+
+var quotes []quote = []quote{
+	{"ABC", "ABCD"},
+}
+
+func main() {
+	rand.Seed(time.Now().Unix())
+
+	api := echo.New()
+
+	api.GET("/", getQuotes)
+	api.GET("/quotes/random", getRandomQuotes)
+
+	port := os.Getenv("Port")
+	if port == "" {
+		port = "32445"
+		//localhost:9090
+	}
+
+	api.Start(":" + port)
+
+}
+
+func getRandomQuotes(c echo.Context) error {
+	index := rand.Intn(len(quotes))
+	return c.JSON(http.StatusOK, quotes[index])
+}
+func getQuotes(c echo.Context) error {
+	return c.JSON(200, quotes)
+}
